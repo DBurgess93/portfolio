8eb85dc52ffce165feaa14d592b59369ddb5f4cf
Daniel Burgess <dan-burgess@hotmail.com>
2023-05-17 16:05:42 +0930

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const Navbar = ({handles}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>
+      <nav>
+        <ul>
+          <li>About Me</li>
+          <li>Projects</li>
+          <li>Contact</li>
+          <li>Resume</li>
+        </ul>
+      </nav>
+      <nav>
+        <ul>
+          <li><a href={handles[0].url} target="_blank" rel="noreferrer">{handles[0].name} </a></li>
+          <li><a href={handles[1].url} target="_blank" rel="noreferrer">{handles[1].name}</a></li>
+          <li><a href={handles[2].url} target="_blank" rel="noreferrer">{handles[2].name}</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+const App = () => {
+  const handles = [
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/daniel-burgess-668176186/"
+    },
+    {
+      name: "GitHub",
+      url: "https://github.com/DBurgess93"
+    },
+    {
+      name: "Email",
+      url: "mailto:dan-burgess@hotmail.com"
+    }
+  ]
+  return (
+    <div>
+      <Navbar handles={handles} />
     </div>
   );
 }
