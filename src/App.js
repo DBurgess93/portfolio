389773d5d9a3e17beae3f4e2f9a736b0767681a1
Daniel Burgess <dan-burgess@hotmail.com>
2023-05-17 16:28:03 +0930

    Add AboutMe component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const Navbar = ({ handles }) => {
       <nav>
         <ul>
           {handles.map(handle => (
-            <li><a href={handle.url} target="_blank" rel="noreferrer">{handle.name} </a></li>
+            <li key={handle.name}><a href={handle.url} target="_blank" rel="noreferrer">{handle.name} </a></li>
           ))}
         </ul>
       </nav>
@@ -27,6 +27,27 @@ const Title = () => {
       <h1>Daniel Burgess</h1>
       <h3>Full-Stack Web developer </h3>
       <p>I like to build minimalistic, functional webpages with a focus on user experience.</p>
+    </div>
+  )
+}
+
+const AboutMe = ({ leWagon, techStack }) => {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>
+        Skilled Full-Stack Developer with a passion for coding, initially as a hobby with The Odin Project, escalating
+        to completing an intense Web-Development bootcamp with <a href={leWagon.url} target="_blank" rel="noreferrer" >{leWagon.name}</a>.
+        Committed to a lifetime of learning, my time now is spent expanding my knowledge in ReactJS, Typescript, design using Figma, and continually building projects to improve on all aspects of Full-Stack Development. <br />
+        Location: Adelaide, South Australia
+      </p>
+      <div>
+        <ul>
+          {techStack.map(item => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
@@ -46,10 +67,27 @@ const App = () => {
       url: "mailto:dan-burgess@hotmail.com"
     }
   ]
+
+  const leWagon = {
+    name: "Le Wagon",
+    url: "https://www.lewagon.com/"
+  }
+
+  const techStack = [
+    "Ruby on Rails",
+    "JavaScript",
+    "ReactJS",
+    "SCSS",
+    "HTML5",
+    "Figma",
+    "PostgreSQL"
+  ]
+
   return (
     <div>
       <Navbar handles={handles} />
       <Title />
+      <AboutMe leWagon={leWagon} techStack={techStack} />
     </div>
   );
 }
